177b022347efba3417490f66c01d0baf47239a3c
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-04-04 21:47:51 +0200

    error.
    Incorrect elements
    of the function in the jsx file, on handleClik

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,21 @@ import { useState } from 'react'
 import './App.css'
 
   function App() {
-     let count = 0;
-     const counterDisplay = document.getElementById('counter');
-     const clickBtn = document.getElementById('clickBtn');
-     const resetBtn = document.getElementById('resetBtn');
+    const [count, setCount] = useState(0);
 
-     clickBtn.addEventListener('click', () => {
-         count++;
-         counterDisplay.textContent = count;
-     });
-
-     resetBtn.addEventListener('click', ()=>{
-         count = 0;
-         counterDisplay.textContent = count;
-     });
+    const handleClick = () => {
+        setCount(prev => prev +1);
+    };
+    const handleReset = () =>{
+        setCount(0);
+    };
 
   return (
     <div className='container'>
-        <h1>click counter</h1>
-        <p>Number of clicks<span id='counter'>0</span></p>
-        <button id='clickBtn'>click me</button>
-        <button id='resetBtn'>Reset</button>
+        <h1>Click counter</h1>
+        <p>Number of clicks<span> {count}</span></p>
+        <button onClick={handleClick}>click me</button>
+        <button onClick={handleReset}>Reset</button>
     </div>
   )
 }
